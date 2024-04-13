bccaa50c233fe8d8e2a3ffd5d60ddff71431a87f
ElsabbahInstitute <ahmad.m.mustapha@hotmail.com>
2024-04-13 15:27:58 +0300

    Setup Search Bar Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,33 @@
 import React from 'react';
-import "./Home.js";
+import "./Home.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
     return (
-        <div>
-            <h2>Welcome to the Main Page</h2>
-            <p>This is the landing page of our application.</p>
+        <div className="banner">
+
+            <img src="./assets/img/banner.jpeg" alt=""/>
+
+            <div className="bannerContent">
+
+                <div className="bannerText">The best online learning resources about hundreds of topics, shared by creators.</div>
+                
+                <div className="searchBox">
+                    <input type="text" placeholder='Search learning resources'></input>
+                    <span>
+                        <FontAwesomeIcon icon={faSearch} size="lg" />
+                    </span>
+                </div>
+
+                <div className='trendingBox'>
+                    <span>Trending:</span>
+                    <span className="trendingItem">Neural Networks</span>
+                    <span className="trendingItem">Attention</span>
+                    <span className="trendingItem">Transformers</span>
+                    <span className="trendingItem">Information Maximization</span>
+                </div>
+            </div>
         </div>
     );
 };
